58d5083882094213a8469cba370e5e1d72607f06
lare <74225315+laresamdeola@users.noreply.github.com>
2022-02-05 09:04:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from turtle import Turtle, Screen
+
+square_turtle = Turtle()
+square_turtle.shape("turtle")
+square_turtle.color("blue")
+
+i = 3
+times = 1
+degrees = 360
+
+while times <= 8:
+    for _ in range(i):
+        if i == 3:
+            square_turtle.forward(100)
+            square_turtle.right(degrees/i)
+            square_turtle.color("black")
+        elif i == 4:
+            square_turtle.forward(100)
+            square_turtle.right(degrees/i)
+            square_turtle.color("blue")
+        elif i == 5:
+            square_turtle.forward(100)
+            square_turtle.right(degrees/i)
+            square_turtle.color("brown")
+        elif i == 6:
+            square_turtle.forward(100)
+            square_turtle.right(degrees/i)
+            square_turtle.color("magenta")
+        elif i == 7:
+            square_turtle.forward(100)
+            square_turtle.right(degrees/i)
+            square_turtle.color("orange")
+        elif i == 8:
+            square_turtle.forward(100)
+            square_turtle.right(degrees/i)
+            square_turtle.color("green")
+        elif i == 9:
+            square_turtle.forward(100)
+            square_turtle.right(degrees/i)
+            square_turtle.color("purple")
+    i += 1
+    times += 1
+
+screen = Screen()
+screen.exitonclick()
